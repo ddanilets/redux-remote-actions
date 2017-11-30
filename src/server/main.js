5f0df7a1e2a8911e9b7b8765eabd675913e32a23
d_danilets <dmytro.danylets@sixt.com>
2017-11-30 16:14:59 +0200

    fix express dep

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,9 @@
-import express from 'express';
 import bodyParser from 'body-parser';
 import httpEnchancer from 'http';
 import ioEnchancer from 'socket.io';
 import websocketHandler from './ws';
 
-export default function (server = express()) {
+export default function (server) {
   const http = httpEnchancer.Server(server);
   const io = ioEnchancer(http, { transports: ['polling', 'websocket'] });
   server.use(bodyParser.json());
